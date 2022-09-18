d74fda753826d304778efdf7581d78ea4bb6d783
starksakthi <103945569+starksakthi@users.noreply.github.com>
2022-09-18 12:03:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+var menulist=document.getElementById("menulist");
+menulist.style.maxHeight="0px";
+function togglemenu(){
+   if (menulist.style.maxHeight=="0px")
+       {
+           menulist.style.maxHeight="590px";
+       }
+       else{
+           menulist.style.maxHeight="0px"
+       }
+}
+ 
